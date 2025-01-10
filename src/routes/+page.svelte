a243f1b8ee454e125d9de364519c9e66f9359048
Scott Koon <skoon@lwsd.wednet.edu>
2025-01-10 12:32:18 -0800

    hooked up click and added pocketbase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+  h1 {
+    background-color: rgb(100, 100, 202);
+    color: white;
+    width:80%;
+    padding: 20px;
+    margin: 20px;
+    text-align:center;
+    border-radius: 0.5em;
+  }
+
+  .container, .camera-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
+<script>
+  function checkOut(camNumber) {
+    alert(camNumber);
+  }
+</script>
+<div class="container">
+  <h1>Camera Check out</h1>
+</div>
+<div class="camera-container">
+  
+  <div>
+    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(5)}}">Camera 5</button>
+    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(2)}}">Camera 2</button>
+    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(10)}}">Camera 10</button>
+    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(17)}}">Camera 17</button>
+    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(21)}}">Camera 21</button>
+  </div>
+  Name: <input type="text" name="checkoutName" id=name>
+</div>
