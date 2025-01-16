20504f8fa928068dda7c76c18b42169c9ea047e0
Scott Koon <skoon@lwsd.wednet.edu>
2025-01-16 15:08:34 -0800

    fixed class switching for buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,8 +16,18 @@
   }
 </style>
 <script>
+  let checkedOut = $state([]);
   function checkOut(camNumber) {
-    var checkoutName = prompt("Name?");
+    
+    let camId = "#cam" + camNumber;
+
+    if(!checkedOut.includes(camId)) {
+      checkedOut.push(camId);
+    } else {
+      let checkoutName = prompt("Name?");
+      let index = checkedOut.indexOf(camId);
+      checkedOut.splice(index,1);
+    }
   }
 </script>
 <div class="container">
@@ -26,10 +36,10 @@
 <div class="camera-container">
   
   <div>
-    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(5)}}">Camera 5</button>
-    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(2)}}">Camera 2</button>
-    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(10)}}">Camera 10</button>
-    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(17)}}">Camera 17</button>
-    <button type="button" class="btn btn-primary" on:click="{()=> { checkOut(21)}}">Camera 21</button>
+    <button type="button" class="btn {checkedOut.includes("#cam5")? "btn-danger":"btn-primary"} " id="cam5" onclick="{()=> { checkOut(5)}}">Camera 5</button>
+    <button type="button" class="btn {checkedOut.includes("#cam2")? "btn-danger":"btn-primary"} " id="cam2" onclick="{()=> { checkOut(2)}}">Camera 2</button>
+    <button type="button" class="btn {checkedOut.includes("#cam10")? "btn-danger":"btn-primary"} " id="cam10" onclick="{()=> { checkOut(10)}}">Camera 10</button>
+    <button type="button" class="btn {checkedOut.includes("#cam17")? "btn-danger":"btn-primary"} " id="cam17" onclick="{()=> { checkOut(17)}}">Camera 17</button>
+    <button type="button" class="btn {checkedOut.includes("#cam21")? "btn-danger":"btn-primary"} " id="cam21" onclick="{()=> { checkOut(21)}}">Camera 21</button>
   </div>
 </div>
